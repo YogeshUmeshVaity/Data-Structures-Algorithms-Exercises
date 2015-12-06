5ae54d56f237ca7484dda987a1822e2a0627c7c7
Yogesh <yogeshvaity@gmail.com>
2015-12-06 19:30:21 +0530

    Finish Ex6 and Ch3

--- Ch3/Ex6/src/com/company/Main.java
+++ Ch3/Ex6/src/com/company/Main.java
@@ -4,53 +4,79 @@ package com.company;
 // demonstrates insertion sort
 // to run this program: C>java InsertSortApp
 //--------------------------------------------------------------
-class ArrayIns
-{
+class ArrayIns {
     private long[] a;                 // ref to array a
     private int nElems;               // number of data items
+
     //--------------------------------------------------------------
     public ArrayIns(int max)          // constructor
     {
         a = new long[max];                 // create the array
         nElems = 0;                        // no items yet
     }
+
     //--------------------------------------------------------------
     public void insert(long value)    // put element into array
     {
         a[nElems] = value;             // insert it
         nElems++;                      // increment size
     }
+
     //--------------------------------------------------------------
     public void display()             // displays array contents
     {
-        for(int j=0; j<nElems; j++)       // for each element,
+        for (int j = 0; j < nElems; j++)       // for each element,
             System.out.print(a[j] + " ");  // display it
         System.out.println("");
     }
+
     //--------------------------------------------------------------
-    public void insertionSort()
-    {
+    public void insertionSort() {
         int in, out;
 
-        for(out=1; out<nElems; out++)     // out is dividing line
+        for (out = 1; out < nElems; out++)     // out is dividing line
         {
             long temp = a[out];            // remove marked item
             in = out;                      // start shifts at out
-            while(in>0 && a[in-1] >= temp) // until one is smaller,
+            while (in > 0 && a[in - 1] >= temp) // until one is smaller,
             {
-                a[in] = a[in-1];            // shift item to right
+                a[in] = a[in - 1];            // shift item to right
                 --in;                       // go left one position
             }
             a[in] = temp;                  // insert marked item
         }  // end for
     }  // end insertionSort()
+
+    public void noDups() {
+        int in, out;
+        int numOfDups = 0;
+        for(out = 1; out < nElems; out++) {
+            long temp = a[out];
+            in = out;
+            while(in > 0) {
+                if(a[in - 1] == temp) {
+                    temp = -1;
+                }
+                if(a[in - 1] >= temp) {
+                    a[in] = a[in - 1];
+                    --in;
+                } else break;
+            }
+            a[in] = temp;
+            if(temp == -1) numOfDups++;
+        }
+        // Shift array to remove -1s
+        nElems = nElems - numOfDups;
+        for(int i = 0; i < nElems; i++) {
+            a[i] = a[i + numOfDups];
+        }
+    }
 //--------------------------------------------------------------
 }  // end class ArrayIns
+
 ////////////////////////////////////////////////////////////////
-class Main
-{
-    public static void main(String[] args)
-    {
+class Main {
+    public static void main(String[] args) {
         int maxSize = 100;            // array size
         ArrayIns arr;                 // reference to array
         arr = new ArrayIns(maxSize);  // create the array
@@ -59,16 +85,21 @@ class Main
         arr.insert(99);
         arr.insert(44);
         arr.insert(55);
-        arr.insert(22);
+        arr.insert(22); //
         arr.insert(88);
         arr.insert(11);
+        arr.insert(55);
+        arr.insert(22);
         arr.insert(00);
         arr.insert(66);
+        arr.insert(44); //
         arr.insert(33);
+        arr.insert(99); //
+        arr.insert(22); //
 
         arr.display();                // display items
 
-        arr.insertionSort();          // insertion-sort them
+        arr.noDups();          // insertion-sort them
 
         arr.display();                // display them again
     }  // end main()
