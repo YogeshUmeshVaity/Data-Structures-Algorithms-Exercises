98f03251c2ac82841210406a8576e2e9f8e922b2
Yogesh <yogeshvaity@gmail.com>
2016-01-25 18:22:45 +0530

    Works for 1 pass, values : 1, 8, 7, 6, 8, 3, 2

--- Ch6/Ex4/src/com/company/Knapsack.java
+++ Ch6/Ex4/src/com/company/Knapsack.java
@@ -6,23 +6,23 @@ import java.util.LinkedList;
  * Created by try on 20/1/16
  */
 public class Knapsack {
-    private int[] weights = {11, 8, 7, 6, 5};
+    private int[] weights = {1, 8, 7, 6, 8, 3, 2};
     private LinkedList<Integer> knapsack = new LinkedList<>();
     private boolean fitted = false;
 
     public void fitItems(int targetWeight, int startingIndex) {
-        for(int i = startingIndex; i < weights.length && !fitted; i++) {
-            if(targetWeight == weights[i]) {
-                knapsack.add(weights[i]);
-                fitted = true;
-                return;
-            } else if(targetWeight > weights[i]) {
-                knapsack.add(weights[i]);
-                fitItems(targetWeight - weights[i], i + 1);
-            }
-            if((i == weights.length - 1 && !fitted)) {
-                knapsack.removeLast();
-            }
+        if(startingIndex == weights.length) {
+            return;
+        }
+        if(targetWeight == weights[startingIndex]) {
+            fitted = true;
+            knapsack.add(weights[startingIndex]);
+            return;
+        } else if(targetWeight > weights[startingIndex]) {
+            fitItems(targetWeight - weights[startingIndex], startingIndex + 1);
+            if(fitted) knapsack.add(weights[startingIndex]);
+        } else if(targetWeight < weights[startingIndex]) {
+            fitItems(targetWeight, startingIndex + 1);
         }
     }
 
@@ -32,6 +32,5 @@ public class Knapsack {
         } else {
             System.out.println("No solution exists");
         }
-
     }
 }
