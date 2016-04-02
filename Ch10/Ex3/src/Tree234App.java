0186b3d82a17f623ce8561839bccdc17e6b85211
Yogesh <yogeshvaity@gmail.com>
2016-04-02 15:43:35 +0530

    Finish Ex3 Ch10.

--- Ch10/Ex3/src/Tree234App.java
+++ Ch10/Ex3/src/Tree234App.java
@@ -1,7 +1,10 @@
 // tree234.java
 // demonstrates 234 tree
 // to run this program: C>java Tree234App
-import java.io.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 ////////////////////////////////////////////////////////////////
 class DataItem
 {
@@ -119,6 +122,8 @@ class Node
 ////////////////////////////////////////////////////////////////
 class Tree234
 {
+    private int sortedArrayIndex = 0;
+    private long[] sortedArray;
     private Node root = new Node();            // make root node
     // -------------------------------------------------------------
     public int find(long key)
@@ -144,15 +149,20 @@ class Tree234
         return current.getMinimumFromNode();
     }
 
-    public void inOrder() {
+    public long[] sortArray(long[] array) {
+        sortedArray = new long[array.length];
+        for(long item : array) {
+            insert(item);
+        }
         inOrderTraverse(root);
+        return sortedArray;
     }
 
     private void inOrderTraverse(Node currentNode) {
         if(currentNode.isLeaf()) { // Base case
             // Display current node.
             for(int i = 0; i < currentNode.getNumItems(); i++) {
-                currentNode.getItem(i).displayItem();
+                sortedArray[sortedArrayIndex++] = currentNode.getItem(i).dData;
             }
             return;
         } else {
@@ -161,7 +171,7 @@ class Tree234
             for(int i = 0; i < numChildren; i++) {
                 inOrderTraverse(currentNode.getChild(i));
                 if(i < numChildren -1) {
-                    currentNode.getItem(i).displayItem();
+                    sortedArray[sortedArrayIndex++] = currentNode.getItem(i).dData;
                 }
             }
         }
@@ -277,73 +287,12 @@ class Tree234
 ////////////////////////////////////////////////////////////////
 class Tree234App
 {
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         long value;
         Tree234 theTree = new Tree234();
-
-        theTree.insert(50);
-        theTree.insert(40);
-        theTree.insert(60);
-        theTree.insert(30);
-        theTree.insert(70);
-
-        while(true)
-        {
-            System.out.print("\nEnter first letter of ");
-            System.out.print("show, insert, minimum, find or traverse: ");
-            char choice = getChar();
-            switch(choice)
-            {
-                case 's':
-                    theTree.displayTree();
-                    break;
-                case 'i':
-                    System.out.print("Enter value to insert: ");
-                    value = getInt();
-                    theTree.insert(value);
-                    break;
-                case 'f':
-                    System.out.print("Enter value to find: ");
-                    value = getInt();
-                    int found = theTree.find(value);
-                    if(found != -1)
-                        System.out.println("Found "+value);
-                    else
-                        System.out.println("Could not find "+value);
-                    break;
-                case 'm':
-                    System.out.println("Minimum element is " + theTree.getMinimum());
-                    break;
-                case 't':
-                    theTree.inOrder();
-                    break;
-                default:
-                    System.out.print("Invalid entry\n");
-            }  // end switch
-        }  // end while
-    }  // end main()
-    //--------------------------------------------------------------
-    public static String getString() throws IOException
-    {
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-        String s = br.readLine();
-        return s;
-    }
-    //--------------------------------------------------------------
-    public static char getChar() throws IOException
-    {
-        String s = getString();
-        return s.charAt(0);
+        long[] array = {90, 60, 45, 120, 30, 70, 102};
+        System.out.println(Arrays.toString(theTree.sortArray(array)));
     }
 
-    //-------------------------------------------------------------
-    public static int getInt() throws IOException
-    {
-        String s = getString();
-        return Integer.parseInt(s);
-    }
-//-------------------------------------------------------------
 }  // end class Tree234App
 ////////////////////////////////////////////////////////////////
